d078031e7919b82bba59b1a7142e900721794481
belen002 <belenberhanu15@gmail.com>
2025-06-23 00:18:12 +0300

    added animation on orange

--- 3D-Product-viewer/scripts/main.js
+++ 3D-Product-viewer/scripts/main.js
@@ -4,10 +4,11 @@ import { addLighting } from "./addLighting.js";
 import { createProduct } from "./createProduct.js";
 import { setupInteraction } from "./interaction.js";
 import { setupCameraAnimation } from "./cameraAnimation.js";
+import * as THREE from "three";
 
 const { scene, camera, renderer, controls } = initScene();
 addLighting(scene);
-createProduct(scene);
+const { plate, orange } = createProduct(scene);
 setupInteraction(renderer, scene, camera,controls); 
 setTimeout(() => {
   camera.position.set(1, 2, 3); // move closer
@@ -17,7 +18,7 @@ setTimeout(() => {
 }, 2000);
 
 const cameraAnimator = setupCameraAnimation(camera, controls);
-
+let clock = new THREE.Clock(); 
 let lastTime = performance.now();
 
 function animate() {
@@ -27,11 +28,17 @@ function animate() {
   const delta = (now - lastTime) / 1000; // seconds
   lastTime = now;
 
+  const elapsed = clock.getElapsedTime(); 
+  if (orange) {
+    orange.position.y = 1.9 + Math.sin(elapsed * 2) * 0.05; // float
+    orange.rotation.y += 0.01; // slow spin
+  }
   controls.update();
-  cameraAnimator.update(delta); // ← add this line to update orbit
+  cameraAnimator.update(delta); // update orbit
 
   renderer.render(scene, camera);
 }
+
 
 animate();
 
